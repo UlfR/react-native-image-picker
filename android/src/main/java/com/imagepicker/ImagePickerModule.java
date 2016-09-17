e72d3a1b2463b913d199fc9aa3105a99854f3014
Gleb Stiblo <errandir@gmail.com>
2016-09-17 16:11:51 +0600

    both pathes

--- android/src/main/java/com/imagepicker/ImagePickerModule.java
+++ android/src/main/java/com/imagepicker/ImagePickerModule.java
@@ -409,6 +409,9 @@ public class ImagePickerModule extends ReactContextBaseJavaModule implements Act
     int initialWidth = options.outWidth;
     int initialHeight = options.outHeight;
 
+    response.putString("uriF", uri.toString());
+    response.putString("pathF", realPath);
+
     // don't create a new file if contraint are respected
     if (((initialWidth < maxWidth && maxWidth > 0) || maxWidth == 0) && ((initialHeight < maxHeight && maxHeight > 0) || maxHeight == 0) && quality == 100 && (rotation == 0 || currentRotation == rotation)) {
       response.putInt("width", initialWidth);
